55c905e23636922a0fe42f00641eb93fc6fa09f3
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/GitPlugin@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/GitPlugin/git.pl
+++ lib/Foswiki/Plugins/GitPlugin/git.pl
@@ -1,23 +1,23 @@
 #!/usr/bin/perl
 
-my $ssh_route = $ARGV[0];
-my $ssh_key = $ARGV[2];
+my $ssh_route   = $ARGV[0];
+my $ssh_key     = $ARGV[2];
 my $FoswikiPath = $ARGV[1];
 
 my $data;
-if ( open (my $IN,'<',$ssh_route) ) {
-	local $/ = undef;
-	$data = <$IN>;
-	$data =~ s#ssh -i (.*?) "\$@"#$1#;
-	close ($IN);
+if ( open( my $IN, '<', $ssh_route ) ) {
+    local $/ = undef;
+    $data = <$IN>;
+    $data =~ s#ssh -i (.*?) "\$@"#$1#;
+    close($IN);
 }
 
 if ( $data ne $ssh_key ) {
-	open (my $OUT,'>',$ssh_route);
-	print $OUT "#!/bin/sh\nssh -i $ssh_key \"\$@\" ";
-	close ($OUT);
+    open( my $OUT, '>', $ssh_route );
+    print $OUT "#!/bin/sh\nssh -i $ssh_key \"\$@\" ";
+    close($OUT);
 }
-chmod(0744,$ssh_route);
+chmod( 0744, $ssh_route );
 
 $ENV{GIT_SSH} = $ssh_route;
 
@@ -27,6 +27,6 @@ chdir $FoswikiPath;
 
 my $execCmd = "$ARGV[3] $ARGV[4]";
 
-print  `$execCmd 2>&1`;
+print `$execCmd 2>&1`;
 
 chdir $start;
